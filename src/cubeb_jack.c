8f628164e3fca87eac7d0e5251401e4ff38501f9
David Richards <rawdod@gmail.com>
2011-12-22 22:02:20 -0500

    cubeb jack support skeleton, WG: This code is non normative

--- src/cubeb_jack.c
+++ src/cubeb_jack.c
@@ -13,20 +13,20 @@ typedef struct jack_St jack_t;
 
 struct jack_St {
 
-	int active;
-	int xruns;
-	const char *server_name;
-	const char *client_name;
-	jack_options_t options;
-	jack_status_t status;
-	jack_client_t *jack_client;
-    void *userdata;
-    
+  int active;
+  int xruns;
+  int callbacks;
+  const char *server_name;
+  const char *client_name;
+  jack_options_t options;
+  jack_status_t status;
+  jack_client_t *jack_client;
+  void *userdata;
+
 };
 
 struct cubeb {
   jack_t *jack;
-  
 };
 
 struct cubeb_stream {
@@ -39,6 +39,35 @@ struct cubeb_stream {
 };
 
 
+int jack_process(jack_nframes_t nframes, void *arg) {
+
+  cubeb *ctx = (cubeb *)arg;
+	
+  ctx->jack->callbacks++;
+	
+  return 0;
+}
+
+
+void jack_shutdown(void *arg) {
+
+  //cubeb *ctx = (cubeb *)arg;
+
+}
+
+int jack_xrun(void *arg)
+{
+
+	cubeb *ctx = (cubeb *)arg;
+
+	ctx->jack->xruns++;
+	
+	printf("Cubeb Jack %s xrun number %d!\n", ctx->jack->client_name, ctx->jack->xruns);
+
+	return 0;
+
+}
+
 /** Initialize an application context.  This will perform any library or
     application scoped initialization.
     @param context
@@ -47,11 +76,58 @@ struct cubeb_stream {
     @retval CUBEB_ERROR */
 int cubeb_init(cubeb ** context, char const * context_name) {
 
+  cubeb *ctx;
+  jack_t *jack;
+	
   ctx = calloc(1, sizeof(*ctx));
 
-                
-  printf("Wanted to Init context: %s\n", context_name);      
+  assert(ctx);
+             
+  printf("Cubeb Jack init context: %s\n", context_name);      
 
+
+  jack = calloc (1, sizeof (jack_t));
+
+  assert(jack);
+
+  jack->client_name = "cubeb";
+
+  jack->server_name = NULL;
+  jack->options = JackNoStartServer;
+
+  jack->userdata = ctx;
+
+	// Connect up to the JACK server 
+
+  jack->jack_client = jack_client_open (jack->client_name, jack->options, &jack->status, jack->server_name);
+  
+  if (jack->jack_client == NULL) {
+    printf("jack_client_open() failed, status = 0x%2.0x\n", jack->status);
+    if (jack->status & JackServerFailed) {
+      printf("Unable to connect to JACK server\n");
+	}
+    exit (1);
+  }
+
+  if (jack->status & JackNameNotUnique) {
+    jack->client_name = jack_get_client_name(jack->jack_client);
+    printf("unique name `%s' assigned\n", jack->client_name);
+  }
+
+  /* Set up Callbacks */
+
+  jack_set_process_callback (jack->jack_client, jack_process, jack->userdata);
+  jack_on_shutdown (jack->jack_client, jack_shutdown, jack->userdata);
+  jack_set_xrun_callback (jack->jack_client, jack_xrun, jack->userdata);
+
+  // Activate
+
+  if (jack_activate (jack->jack_client)) {
+    printf("cannot activate client\n");
+    exit (1);
+  }
+
+  ctx->jack = jack;
   *context = ctx;
   
   return CUBEB_OK;
@@ -62,80 +138,56 @@ int cubeb_init(cubeb ** context, char const * context_name) {
     @param context */
 void cubeb_destroy(cubeb * context) {
 
+  printf("Cubeb Jack destroy context\n");      
+
+  printf("%d callbacks, %d xruns.\n", context->jack->callbacks, context->jack->xruns);
+	
+  jack_client_close (context->jack->jack_client);
+  free(context->jack);
   free(context);
 
 }
 
-/** Initialize a stream associated with the supplied application context.
-    @param context
-    @param stream
-    @param stream_name
-    @param stream_params
-    @param latency Approximate stream latency in milliseconds.  Valid range is [1, 2000].
-    @param data_callback Will be called to preroll data before playback is
-                          started by cubeb_stream_start.
-    @param state_callback
-    @param user_ptr
-    @retval CUBEB_OK
-    @retval CUBEB_ERROR
-    @retval CUBEB_ERROR_INVALID_FORMAT */
+
 int cubeb_stream_init(cubeb * context, cubeb_stream ** stream, char const * stream_name,
                       cubeb_stream_params stream_params, unsigned int latency,
                       cubeb_data_callback data_callback,
                       cubeb_state_callback state_callback,
                       void * user_ptr) {
                 
-  printf("Wanted to Init stream\n");      
+  printf("Cubeb Jack init stream\n");      
                       
   return CUBEB_OK;
                       
 }
 
-/** Destroy a stream.
-    @param stream */
 void cubeb_stream_destroy(cubeb_stream * stream) {
 
-  printf("Wanted to destroy stream\n");      
+  printf("Cubeb Jack destroy stream\n");      
                       
   return CUBEB_OK;
 }
 
-/** Start playback.
-    @param stream
-    @retval CUBEB_OK
-    @retval CUBEB_ERROR */
 int cubeb_stream_start(cubeb_stream * stream) {
 
-  printf("Wanted to start stream\n");      
+  printf("Cubeb Jack start stream\n");      
                       
   return CUBEB_OK;
 
 }
 
-/** Stop playback.
-    @param stream
-    @retval CUBEB_OK
-    @retval CUBEB_ERROR */
 int cubeb_stream_stop(cubeb_stream * stream) {
 
-  printf("Wanted to stop stream\n");      
+  printf("Cubeb Jack stop stream\n");      
                       
   return CUBEB_OK;
-  
 
 }
 
-/** Get the current stream playback position.
-    @param stream
-    @param position Playback position in frames.
-    @retval CUBEB_OK
-    @retval CUBEB_ERROR */
 int cubeb_stream_get_position(cubeb_stream * stream, uint64_t * position) {
 
-
-  printf("Wanted to get stream position\n");      
+  printf("Cubeb Jack stream position\n");      
                       
   return CUBEB_OK;
-
 
 }
